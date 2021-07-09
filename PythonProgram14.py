9240e18e56037a2eb791f421bd2a675eec5874bb
Tanzina Rahman Mona <77913975+TanzinaRahman@users.noreply.github.com>
2021-07-10 01:40:10 +0600

    Create PythonProgram14.py

---
+++ PythonProgram14.py
@@ -0,0 +1,12 @@
+# Formatting String & Type Function
+num = 20
+print(type(num))
+num2 = 80.4
+print(type(num2))
+name = 'tanzina'
+print(type(name))
+print(type(True))
+print(type(False))
+print(f"{num} + {num2} = {num+num2} ")
+print("Tanzina Rahman Mona.", end=" ")
+print("01717134388")
